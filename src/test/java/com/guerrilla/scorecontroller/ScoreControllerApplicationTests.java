5dd03c1420c1bf7a6a23569017e48ba2476ce695
Tomas Sirio <tsirio@bol.com>
2023-11-03 11:08:31 +0100

    First Commit

---
+++ src/test/java/com/guerrilla/scorecontroller/ScoreControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerrilla.scorecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
