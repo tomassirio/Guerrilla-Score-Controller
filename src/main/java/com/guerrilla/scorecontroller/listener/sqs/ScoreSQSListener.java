46a6de246028625f0fcfa84d8280bafa1d50d8a6
Tomas Sirio <tsirio@bol.com>
2023-11-05 21:39:50 +0100

    Hook Sqs to Player Service

--- src/main/java/com/guerrilla/scorecontroller/listener/sqs/ScoreSQSListener.java
+++ src/main/java/com/guerrilla/scorecontroller/listener/sqs/ScoreSQSListener.java
@@ -2,19 +2,33 @@ package com.guerrilla.scorecontroller.listener.sqs;
 
 import com.guerrilla.scorecontroller.dto.ScoreDto;
 import com.guerrilla.scorecontroller.listener.ScoreListener;
+import com.guerrilla.scorecontroller.model.Score;
+import com.guerrilla.scorecontroller.service.PlayerService;
+import com.guerrilla.scorecontroller.service.ScoreService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Slf4j
 @Component
 public class ScoreSQSListener implements ScoreListener {
+
+    private final ScoreService scoreService;
+
+    @Autowired
+    public ScoreSQSListener(ScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
+
     @Override
     @SqsListener(value = "https://sqs.eu-west-2.amazonaws.com/765122112100/scores", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void consume(@NotificationMessage ScoreDto scoreDto) {
-        log.info("Received score in listener from Player: " + scoreDto.playerId());
-
+        Score score = scoreService.createScore(UUID.fromString(scoreDto.playerId()), scoreDto.value());
+        log.info("Score Created through Listener: " + score.getScoreId());
     }
 }
